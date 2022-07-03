440056551d16a48363bb2f19b5367372e8816842
lola1234 <101635306+lola1234@users.noreply.github.com>
2022-07-03 03:23:21 -0400

    completed import of weather search project from Codesandbox

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Weather from "./Weather.js";
+import "./styles.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <Weather />
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather App</h1>
     </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.js";
+import reportWebVitals from "./reportWebVitals";
+import Weather from "./Weather.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+    <Weather />
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
