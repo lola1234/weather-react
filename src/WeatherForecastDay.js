22e8dd473e8cd883b2742ccf2f39d68685fcce3a
lola1234 <101635306+lola1234@users.noreply.github.com>
2022-07-17 03:02:02 -0400

    Refactored code, fixed warnings

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -22,14 +22,14 @@ export default function WeatherForecastDay(props) {
 
   return (
     <div>
-      <div className="WeatherForecast-day">{props.data.dt[0].dt}</div>
-      <WeatherIcon code={props.data.dt[0].weather[0].icon} size={36} />
+      <div className="WeatherForecast-day">{day()}</div>
+      <WeatherIcon code={props.data.weather[0].icon} size={36} />
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperature-max">
-          {maxTemperature()}°
+          {maxTemperature()}
         </span>
         <span className="WeatherForecast-temperature-min">
-          {minTemperature()}°
+          {minTemperature()}
         </span>
       </div>
     </div>
